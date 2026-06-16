c15afe6f86f463195ebceab239bf9c05a1222c9a
Lê Hải Nguyên <lehainguyen876@gmail.com>
2026-06-16 18:44:08 +0700

    Delete Session 23/HCM_KS25_CNTT7_python_Session20_EX05/utils directory

--- Session 23/HCM_KS25_CNTT7_python_Session20_EX05/utils/player_utils.py
+++
@@ -1,6 +0,0 @@
-def find_player(records, player_id):
-    clean_id = player_id.strip().upper()
-    for player in records:
-        if player["player_id"] == clean_id:
-            return player
-    return None
